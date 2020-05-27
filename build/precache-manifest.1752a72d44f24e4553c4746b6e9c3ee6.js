1f16892948cae88f97080b0738919c969c956826
Srikar Gouru <srikarg89@gmail.com>
2020-05-27 17:53:56 -0400

    Push build folder

---
+++ build/precache-manifest.1752a72d44f24e4553c4746b6e9c3ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0245f7c4411a114bc3c7688700548ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c707a31844fb171207bc",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ad8ab312a1a59c0cd260",
+    "url": "/static/js/2.86318a5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86318a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c707a31844fb171207bc",
+    "url": "/static/js/main.2c0635ff.chunk.js"
+  },
+  {
+    "revision": "911896b14f4a4f7a8a25",
+    "url": "/static/js/runtime-main.853cd8c3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
