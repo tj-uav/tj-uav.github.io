2f06797f7af8c9bbf8b6616396c4af6d0b7378e9
Jklein64 <jklein.64@verizon.net>
2020-07-27 14:45:37 -0400

    Make hero section (somewhat) responsive

--- src/pages/home/content/Hero.js
+++ src/pages/home/content/Hero.js
@@ -2,53 +2,93 @@ import React from "react"
 import styled from "styled-components"
 import Image from "components/Image"
 import Grid from "components/Grid"
-import { Subheading } from "theme/Styles"
+import { Subheading, Breakpoints } from "theme/Styles"
 import { darker } from "theme/Colors"
 
-// prettier-ignore
 const Container = styled(Grid)`
-	--rows: repeat(18, 1fr);
-	grid-template-areas: 
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     logo  logo  logo  logo  logo  .     image image image image .     "
-	".     logo  logo  logo  logo  logo  .     image image image image .     "
-	".     logo  logo  logo  logo  logo  .     image image image image .     "
-	".     .     .     .     .     .     .     image image image image .     "
-	".     text  text  text  text  text  .     image image image image .     "
-	".     text  text  text  text  text  .     image image image image .     "
-	".     text  text  text  text  text  .     image image image image .     "
-	".     text  text  text  text  text  .     image image image image .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     "
-	".     .     .     .     .     .     .     .     .     .     .     .     ";
-
 	background-color: ${darker};
+
+	${Breakpoints.small} {
+		--rows: repeat(13, 2.1875rem) auto;
+		--columns: unset;
+
+		height: initial;
+		padding: 0 1rem;
+
+		/* prettier-ignore */
+		grid-template-areas:
+			" .     "
+			" .     "
+			" .     "
+			" logo  "
+			" logo  "
+			" .     "
+			" .     "
+			" image "
+			" image "
+			" image "
+			" image "
+			" image "
+			" .     "
+			" text  ";
+	}
+
+	${Breakpoints.desktop} {
+		--rows: repeat(18, 1fr);
+
+		/* prettier-ignore */
+		grid-template-areas: 
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     logo  logo  logo  logo  logo  .     image image image image .     "
+		".     logo  logo  logo  logo  logo  .     image image image image .     "
+		".     logo  logo  logo  logo  logo  .     image image image image .     "
+		".     .     .     .     .     .     .     image image image image .     "
+		".     text  text  text  text  text  .     image image image image .     "
+		".     text  text  text  text  text  .     image image image image .     "
+		".     text  text  text  text  text  .     image image image image .     "
+		".     text  text  text  text  text  .     image image image image .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     "
+		".     .     .     .     .     .     .     .     .     .     .     .     ";
+	}
+`
+
+const logoProps = {
+	src: require("pages/home/assets/logo.svg"),
+	style: { gridArea: "logo" },
+	alt: "TJUAV Logo",
+}
+
+const heroProps = {
+	src: require("pages/home/assets/img1.jpeg"),
+	style: { width: "100%", height: "100%", objectFit: "cover" },
+	alt: "The team",
+	border: true,
+}
+
+const ThemedSubheading = styled.h2(() => Subheading)
+const StyledSubheading = styled(ThemedSubheading)`
+	align-self: flex-end;
+	grid-area: text;
+`
+
+const HeroContainer = styled.div`
+	grid-area: image;
 `
 
 const Hero = ({ content, ...props }) => (
 	<Container {...props}>
-		<Image
-			src={require("pages/home/assets/logo.svg")}
-			style={{ gridArea: "logo" }}
-			alt="TJUAV Logo"
-		/>
-		<h2 style={{ ...Subheading, gridArea: "text", alignSelf: "flex-end" }}>
-			{content.description}
-		</h2>
-		<div style={{ gridArea: "image" }}>
-			<Image
-				src={require("pages/home/assets/img1.jpeg")}
-				style={{ width: "100%", height: "100%", objectFit: "cover" }}
-				alt="The team"
-				border
-			/>
-		</div>
+		<Image {...logoProps} />
+		<StyledSubheading>{content.description}</StyledSubheading>
+		<HeroContainer>
+			<Image {...heroProps} />
+		</HeroContainer>
 	</Container>
 )
 
