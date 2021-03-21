58ff3a2b5a9364abd85fc60caa7c6d6548bb8805
Jklein64 <jklein.64@verizon.net>
2021-03-21 11:21:56 -0400

    Commit build directory so gh-pages can find it

--- src/theme/Breakpoints.js
+++
@@ -1,3 +0,0 @@
-export const mobile = `@media only screen and (min-width: ${0}px)`
-export const tablet = `@media only screen and (min-width: ${480}px)`
-export const desktop = `@media only screen and (min-width: ${860}px)`
--- src/theme/Colors.js
+++
@@ -1,5 +0,0 @@
-export const dark = "#2E3842"
-export const red = "#FF5252"
-export const text = "#FFFFFF"
-export const blue = "#346CBC"
-export const darker = "#1D242A"
