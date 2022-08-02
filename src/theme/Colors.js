254a5d3ef758a9c624146ed6e0eb4d04327a9d56
william blakc <williamblack7586@gmail.com>
2022-08-02 12:40:31 -0400

    Change blue in colors

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,5 +1,5 @@
 export const dark = "#2E3842"
 export const red = "#FF5252"
 export const text = "#FFFFFF"
-export const blue = "#346CBC"
+export const blue = "#4173ba"
 export const darker = "#1D242A"
